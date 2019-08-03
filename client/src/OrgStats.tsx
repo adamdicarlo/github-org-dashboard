974b4ecc71c52c8f498ce6201cd09cfae0640340
Adam DiCarlo <adam.dicarlo@gmail.com>
2019-08-03 13:21:47 -0700

    :sparkles: Show separate tables for each stat

--- client/src/OrgStats.tsx
+++ client/src/OrgStats.tsx
@@ -6,11 +6,16 @@ interface APIError {
   message?: string
 }
 
+// A map of repository name -> number of contributors.
+type OrderedRepos = {
+  name: string
+  count: number
+}[]
+
 // View model and API response format.
 interface BasicStats {
-  forks: number
-  repos: number
-  stars: number
+  byForks: OrderedRepos
+  byStars: OrderedRepos
 }
 
 // API response format for errors.
@@ -24,12 +29,12 @@ interface Props {
 
 const OrgStats: React.FC<Props> = ({ name }) => {
   const [basicStats, setBasicStats] = useState<BasicStats | null>(null)
+  const [mostContributors, setMostContributors] = useState<OrderedRepos | null>(null)
   const [error, setError] = useState<APIError | null>(null)
 
   useEffect(() => {
     (async (): Promise<void> => {
       let response
-      
       try {
         const uriName = encodeURIComponent(name)
         response = await fetch(`/orgs/${uriName}/basic-stats`)
@@ -47,16 +52,27 @@ const OrgStats: React.FC<Props> = ({ name }) => {
     })()
   }, [name])
 
-  if (basicStats) {
-    const { forks, repos, stars } = basicStats
-    return (
-      <>
-        <p>Stars: {stars}</p>
-        <p>Forks: {forks}</p>
-        <p>Repos: {repos}</p>
-      </>
-    )
-  } else if (error) {
+  useEffect(() => {
+    (async (): Promise<void> => {
+      let response
+      try {
+        const uriName = encodeURIComponent(name)
+        response = await fetch(`/orgs/${uriName}/contributors`)
+        if (response.status !== 200) {
+          setError({ status: response.status })
+          const e: ErrorResponse = await response.json()
+          return setError({ status: response.status, message: e.error })
+        }
+
+        const incoming = await response.json()
+        setMostContributors(incoming)
+      } catch (err) {
+        setError({ message: err.message })
+      }
+    })()
+  }, [name])
+
+  if (error) {
     const { message, status } = error
     return (
       <>
@@ -65,10 +81,58 @@ const OrgStats: React.FC<Props> = ({ name }) => {
       </>
     )
   } else {
+    return <>
+      <div>{renderBasicStats(basicStats)}</div>
+      <div>{renderMostContributors(mostContributors)}</div>
+    </>
+  }
+}
+
+function renderBasicStats(basicStats: BasicStats | null) {
+  if (basicStats) {
+    const { byForks, byStars } = basicStats
     return (
-      <h2>Loading...</h2>
+      <div>
+        <p>{renderRepoTable('Stars', byStars)}</p>
+        <p>{renderRepoTable('Forks', byForks)}</p>
+      </div>
     )
   }
+  return <h3>Loading...</h3>
+}
+
+function renderMostContributors(byContributors: OrderedRepos | null) {
+  if (byContributors) {
+    return (
+      <p>
+        {renderRepoTable('Contributors', byContributors)}
+      </p>
+    )
+  }
+  return <h3>Loading...</h3>
+}
+
+function renderRepoTable(propName: string, repos: OrderedRepos) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Repository</th>
+          <th>{propName}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {repos.map(repo => {
+          return (
+            <tr>
+              <td>{repo.name}</td>
+              <td>{repo.count}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
 
 export default OrgStats
--- server/src/index.js
+++ server/src/index.js
@@ -9,6 +9,7 @@ const Octokit = require('@octokit/rest')
 const express = require('express')
 const app = express()
 
+const MAX_RESULTS = 20
 const port = process.env.PORT
 const githubToken = process.env.GITHUB_API_TOKEN
 
@@ -31,6 +32,25 @@ app.get('/orgs/:org/basic-stats', async (req, res) => {
   res.send(reposToBasicStats(githubResponse.data))
 })
 
+app.get('/orgs/:org/contributors', async (req, res) => {
+  const { org } = req.params
+  let repoContributors
+
+  try {
+    const githubResponse = await octokit.repos.listForOrg({ org })
+
+    repoContributors = await Promise.all(
+      githubResponse.data.map(repo => getContributors(org, repo.name))
+    )
+  } catch (error) {
+    const message =
+      error.status === 404 ? 'Organization not found' : error.name || ''
+    return void res.status(error.status || 500).send({ error: message })
+  }
+
+  res.send(formatContributors(repoContributors))
+})
+
 app.listen(port, err => {
   if (err) {
     throw err
@@ -38,19 +58,37 @@ app.listen(port, err => {
   console.log(`Listening on port ${port}`)
 })
 
+async function getContributors (owner, repo) {
+  const { data } = await octokit.repos.listContributors({
+    owner,
+    repo
+  })
+  return { name: repo, count: data.length }
+}
+
+function formatContributors (repoContributorsPairs) {
+  const byContributors = repoContributorsPairs
+    .sort(compareCount)
+    .slice(0, MAX_RESULTS)
+
+  return byContributors
+}
+
 function reposToBasicStats (repos) {
   const stats = {
-    stars: 0,
-    forks: 0,
-    repos: repos.length
+    byForks: repos
+      .map(repo => ({ name: repo.name, count: repo.stargazers_count }))
+      .sort(compareCount)
+      .slice(0, MAX_RESULTS),
+    byStars: repos
+      .map(repo => ({ name: repo.name, count: repo.forks_count }))
+      .sort(compareCount)
+      .slice(0, MAX_RESULTS)
   }
 
-  repos.forEach(({ fork, forks_count, stargazers_count }) => {
-    if (!fork) {
-      stats.forks += forks_count
-      stats.stars += stargazers_count
-    }
-  })
-
   return stats
+}
+
+function compareCount (a, b) {
+  return b.count - a.count
 }
