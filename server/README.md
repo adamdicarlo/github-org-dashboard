158f272029a82f11ba86c09a48c1447ae5425e52
Adam DiCarlo <adam.dicarlo@gmail.com>
2019-08-03 10:14:10 -0700

    :wrench: Add dotenv to server

---
+++ server/README.md
@@ -0,0 +1,8 @@
+# GitHub Org Dashboard Server
+
+## Setup
+
+1. Get a GitHub API token.
+2. Copy `.env.template` to `.env` and edit the file to use your GitHub API token.
+3. Run `yarn` to install dependencies.
+4. Run `yarn start` to start the server.
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,22 @@
+(function () {
+  const result = require('dotenv').config()
+  if (result.error) {
+    throw result.error
+  }
+})()
+
 const express = require('express')
 const app = express()
 
-app.get('/org', (req, res) => {
+const port = process.env.PORT
 
+app.get('/', (req, res) => {
+  res.sendStatus(200)
 })
 
+app.listen(port, (err) => {
+  if (err) {
+    throw err
+  }
+  console.log(`Listening on port ${port}`)
+})
